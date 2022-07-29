58f5ca1034c44e0f0c0cacb20e38c4619dd82a44
Sinkovskyy <viktorsink202@gmail.com>
2022-07-30 01:12:46 +0300

    Game table styles was fixed

--- src/modules/GameTable/GameTable.tsx
+++ src/modules/GameTable/GameTable.tsx
@@ -1,1 +1,23 @@
-import { FC } from 'react'import { FlexWrapper } from '../../components'import { LetterBlock } from './styled'const GameTable: FC = () => {  const arr = [    ['', '', '', '', ''],    ['', '', '', '', ''],    ['', '', '', '', ''],    ['', '', '', '', ''],    ['', '', '', '', ''],    ['', '', '', '', ''],  ]  return (    <FlexWrapper wrap="wrap" gap={5} width="290px">      {arr.map((row) =>        row.map((letter, key) => <LetterBlock key={key}>{letter}</LetterBlock>)      )}    </FlexWrapper>  )}export default GameTable
+import { FC } from 'react'
+import { Container, LetterBlock } from './styled'
+
+const GameTable: FC = () => {
+  const arr = [
+    ['', '', '', '', ''],
+    ['', '', '', '', ''],
+    ['', '', '', '', ''],
+    ['', '', '', '', ''],
+    ['', '', '', '', ''],
+    ['', '', '', '', ''],
+  ]
+
+  return (
+    <Container>
+      {arr.map((row) =>
+        row.map((letter, key) => <LetterBlock key={key}>{letter}</LetterBlock>)
+      )}
+    </Container>
+  )
+}
+
+export default GameTable
--- src/modules/GameTable/styled.ts
+++ src/modules/GameTable/styled.ts
@@ -1,1 +1,47 @@
-import styled from 'styled-components'import { Colors, font } from '../../styles'export const LetterBlock = styled.div`  width: 50px;  height: 50px;  border: 2px solid ${Colors.grey_tone_1};  display: flex;  align-items: center;  justify-content: center;  ${font({ size: '2rem', weight: '700' })}  background-color: transparent;  &.grey {    background-color: ${Colors.grey};  }  &.yellow {    background-color: ${Colors.yellow};  }  &.green {    background-color: ${Colors.green};  }  &.grey,  &.yellow,  &.green {    border: none;  }`
+import styled from 'styled-components'
+import { FlexWrapper } from '../../components'
+import { Colors, font } from '../../styles'
+
+export const Container = styled(FlexWrapper)`
+  max-width: 100%;
+  width: 350px;
+  height: 420px;
+  padding: 10px;
+  flex-wrap: wrap;
+  gap: 5px;
+`
+
+export const LetterBlock = styled.div`
+  width: 62px;
+  height: 62px;
+  border: 2px solid ${Colors.grey_tone_1};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  ${font({ size: '2rem', weight: '700' })}
+
+  background-color: transparent;
+
+  &.grey {
+    background-color: ${Colors.grey};
+  }
+
+  &.yellow {
+    background-color: ${Colors.yellow};
+  }
+
+  &.green {
+    background-color: ${Colors.green};
+  }
+
+  &.grey,
+  &.yellow,
+  &.green {
+    border: none;
+  }
+
+  @media (max-width: 350px) {
+    width: calc(100% * (1 / 5) - 10px - 1px);
+  }
+`
