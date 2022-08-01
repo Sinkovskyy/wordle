67b65d1f49bd41383e02b174323884dc28b5a49e
Sinkovskyy <viktorsink202@gmail.com>
2022-08-01 09:42:02 +0300

    Refator unit test codes

--- src/test-utils.tsx
+++ src/test-utils.tsx
@@ -2,24 +2,11 @@ import React from 'react'
 import { PersistGate } from 'redux-persist/integration/react'
 import { render } from '@testing-library/react'
 import { Provider } from 'react-redux'
-import { shallow } from 'enzyme'
 
 import { store, persistor } from './store'
 
 export function renderWithContext(element: React.ReactElement) {
   const component = render(
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        {element}
-      </PersistGate>
-    </Provider>
-  )
-
-  return { store, component }
-}
-
-export function shallowWithContext(element: React.ReactElement) {
-  const component = shallow(
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         {element}
